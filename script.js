2b1038c989280b4f133987069494271444bf69ce
Prabh Singh <101253649+prabhsingh14@users.noreply.github.com>
2023-07-31 17:57:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,187 @@
+//Variables
+const searchbar = document.querySelector(".searchbar-container");
+const profilecontainer = document.querySelector(".profile-container");
+const root = document.documentElement.style;
+const get = (parameter) => document.getElementById(`${parameter}`);
+const url = "https://api.github.com/users/";
+const noresults = get("no-results");  //document.getElementById("no-results");
+const btnmode = get("btn-mode");
+const modetext = get("mode-text");
+const modeicon = get("mode-icon");
+const btnsubmit = get("submit");
+const input = get("input");
+const avatar = get("avatar");
+const userName = get("name");
+const user = get("user");
+const date = get("date");
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const bio = get("bio");
+const repos = get("repos");
+const followers = get("followers");
+const following = get("following");
+const user_location = get("location");
+const page = get("page");
+const twitter = get("twitter");
+const company = get("company");
+let darkMode = false;
+
+// Event Listeners
+btnsubmit.addEventListener("click", function () {
+  if (input.value !== "") {  // input event is fired when value of <input>, <select>, <textarea> is changed.
+    getUserData(url + input.value);
+  }
+});
+
+input.addEventListener(
+  "keydown", // it is fired when a key is pressed e.g. enter.
+  function (e) {
+    if (e.key == "Enter") {
+      if (input.value !== "") {
+        getUserData(url + input.value);
+      }
+    }
+  },
+  false
+);
+
+input.addEventListener("input", function () {
+  noresults.style.display = "none"; // when no result appears, then on next search it must disappears
+});
+
+btnmode.addEventListener("click", function () {
+  if (darkMode == false) {
+    darkModeProperties();   // now, you want it to activate
+  } else {
+    lightModeProperties();
+  }
+});
+
+// Functions
+
+//API CALL
+function getUserData(gitUrl) {
+  fetch(gitUrl)           // then code will work when promise is settled.
+    .then((response) => response.json())       // then works similar to await, only diff: then not allow to execute code that is in its bracket.
+                                               // whereas await not allow the all upcoming code.
+    .then((data) => {
+      console.log(data);
+      updateProfile(data);
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+
+//RENDER
+function updateProfile(data) {
+  if (data.message !== "Not Found") {  // user not found
+    noresults.style.display = "none";
+    function checkNull(parameter1, parameter2) {
+      if (parameter1 === "" || parameter1 === null) {
+        parameter2.style.opacity = 0.5;
+        parameter2.previousElementSibling.style.opacity = 0.5;
+        return false;
+      } else {
+        return true;
+      }
+    }
+    avatar.src = `${data.avatar_url}`;
+    userName.innerText = data.name === null ? data.login : data.name;   //ternary operator
+    user.innerText = `@${data.login}`;
+    user.href = `${data.html_url}`;
+    datesegments = data.created_at.split("T").shift().split("-");
+    date.innerText = `Joined ${datesegments[2]} ${months[datesegments[1] - 1]} ${datesegments[0]}`;
+    bio.innerText = data.bio == null ? "This profile has no bio" : `${data.bio}`;
+    repos.innerText = `${data.public_repos}`;
+    followers.innerText = `${data.followers}`;
+    following.innerText = `${data.following}`;
+    user_location.innerText = checkNull(data.location, user_location) ? data.location : "Not Available";
+    page.innerText = checkNull(data.blog, page) ? data.blog : "Not Available";
+    page.href = checkNull(data.blog, page) ? data.blog : "#";
+    twitter.innerText = checkNull(data.twitter_username, twitter) ? data.twitter_username : "Not Available";
+    twitter.href = checkNull(data.twitter_username, twitter) ? `https://twitter.com/${data.twitter_username}` : "#";
+    company.innerText = checkNull(data.company, company) ? data.company : "Not Available";
+    searchbar.classList.toggle("active");
+    profilecontainer.classList.toggle("active");
+  } else {
+    noresults.style.display = "block";
+  }
+}
+
+
+
+//SWITCH TO DARK MODE - activateDarkMode()
+function darkModeProperties() {
+  root.setProperty("--lm-bg", "#141D2F");
+  root.setProperty("--lm-bg-content", "#1E2A47");
+  root.setProperty("--lm-text", "white");
+  root.setProperty("--lm-text-alt", "white");
+  root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+  modetext.innerText = "LIGHT";
+  modeicon.src = "./assets/images/sun-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(1000%)");
+  darkMode = true;
+  console.log("darkmode changed to " + darkMode);
+  localStorage.setItem("dark-mode", true);  console.log("setting dark mode to false");
+
+  console.log("setting dark mode to true");
+
+}
+
+//localStorage: Data stored in localStorage is accessible across all tabs and windows from the same origin (i.e., same domain and protocol). 
+//It has a global scope and remains available even after the browser is closed and reopened.
+//sessionStorage: Data stored in sessionStorage is only accessible within the same tab or window from the same origin (like localStorage). 
+//However, the data is available only for the duration of that session. When the tab or window is closed or the browser is terminated, 
+//the data is cleared.
+
+//SWITCH TO LIGHT MODE - activateLightMode()
+function lightModeProperties() {
+  root.setProperty("--lm-bg", "#F6F8FF");
+  root.setProperty("--lm-bg-content", "#FEFEFE");
+  root.setProperty("--lm-text", "#4B6A9B");
+  root.setProperty("--lm-text-alt", "#2B3442");
+  root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+  modetext.innerText = "DARK";
+  modeicon.src = "./assets/images/moon-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(100%)");
+  darkMode = false;
+  console.log("darkmode changed to " + darkMode);
+
+  localStorage.setItem("dark-mode", false);
+  console.log("setting dark mode to false");
+}
+
+
+//INITIALISE UI
+function init() {
+  //initialise dark-mode variable to false;
+  //darkMode = true -> dark mode enable karna h 
+  //darMode = false -> light mode enable karna h 
+  darkMode = false;
+
+  //HW
+// const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const value = localStorage.getItem("dark-mode");
+
+  if(value === null) {
+    console.log("null k andar");
+    localStorage.setItem("dark-mode", darkMode);
+    lightModeProperties();
+  }
+  else if(value == "true") {
+    console.log("truer k andar");
+    darkModeProperties();
+  }
+  else if(value == "false") {
+    console.log("false k andar");
+    lightModeProperties();
+  }
+
+
+  //by default
+  getUserData(url + "prabhsingh14");
+}
+
+init();
